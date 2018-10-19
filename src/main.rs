76b38e66edc3b9a1d6f77531a04fc9f93dfafc1b
Terkwood <metaterkhorn@gmail.com>
2018-10-19 10:14:45 -0400

    read a line

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        // Read line from standard input
+        let line = read_line();
+
+        // "parse" line into executable command
+        // execute the command in a sep process
+        // show output
+    }
+}
+
+fn read_line() -> String {
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+    input
 }
