63f0ddd8d7ff75850a2d931bd22338e2fe7448e3
Terkwood <metaterkhorn@gmail.com>
2018-10-19 10:25:01 -0400

    first cut

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::io;
+use std::process::Command;
 
 fn main() {
     loop {
@@ -6,8 +7,16 @@ fn main() {
         let line = read_line();
 
         // "parse" line into executable command
+        // ... TODO ...
+
         // execute the command in a sep process
+        let output = sys_call(line);
+
         // show output
+        match output.status.success() {
+            true => println!("{}", std::str::from_utf8(&output.stdout[..]).unwrap()),
+            _ => panic!(),
+        }
     }
 }
 
@@ -15,4 +24,12 @@ fn read_line() -> String {
     let mut input = String::new();
     io::stdin().read_line(&mut input).unwrap();
     input
+}
+
+fn sys_call(command: String) -> std::process::Output {
+    Command::new("sh")
+        .arg("-c")
+        .arg(command)
+        .output()
+        .expect("failed to execute process")
 }
